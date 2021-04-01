55d54d3e87f14c323f3bb5ee8ed99b9486e71926
yoweiliu <liu1257cc@gmail.com>
2021-04-01 21:12:12 +0800

    test

--- arima.py
+++ arima.py
@@ -1,12 +1,12 @@
-import datetime
 '''
 Descripttion: 
 version: 
 Author: liu1257cc
 Date: 2021-04-01 18:36:43
 LastEditors: Please set LastEditors
-LastEditTime: 2021-04-01 20:29:26
+LastEditTime: 2021-04-01 21:11:01
 '''
+import datetime
 
 starttime = datetime.datetime.now()
 #long running

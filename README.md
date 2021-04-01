6e940b6d2464df109b72b40b52bf5f463ab77df4
yoweiliu <liu1257cc@gmail.com>
2021-04-01 20:29:26 +0800

    first

--- README.md
+++ README.md
@@ -4,10 +4,8 @@
  * @Author: liu1257cc
  * @Date: 2021-04-01 17:20:21
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-04-01 17:33:16
+ * @LastEditTime: 2021-04-01 20:09:10
 -->
 # test
------
-ji3g4
-ji3jiiji3jijij3;pls;fl;l;l;hl;hl;
-hlflhklkflhkl
+
+酷喔ijijij
---
+++ arima.py
@@ -0,0 +1,17 @@
+import datetime
+'''
+Descripttion: 
+version: 
+Author: liu1257cc
+Date: 2021-04-01 18:36:43
+LastEditors: Please set LastEditors
+LastEditTime: 2021-04-01 20:16:23
+'''
+
+starttime = datetime.datetime.now()
+#long running
+
+
+
+endtime = datetime.datetime.now()
+print (endtime - starttime)
